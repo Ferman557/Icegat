2cb11d75503a87f9f9ec9d3df4736377d7496ec1
Ferman <Ferman557@gmail.com>
2022-11-20 03:33:03 +0400

    Update functino.js

--- docs/functino.js
+++ docs/functino.js
@@ -1,3 +1,4 @@
+var urlOfCachedPage = 'https://ferman557.github.io/Icegat/';
 var xhr = new XMLHttpRequest();
 xhr.open("GET", urlOfCachedPage, true);
 xhr.setRequestHeader("Cache-Control", "max-age=0");
