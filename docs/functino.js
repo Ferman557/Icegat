3c5234bb78f81732146e29bbc3fb87f345ddfc64
Ferman <Ferman557@gmail.com>
2022-11-21 08:30:04 +0400

    delete all js and editing some html css

--- docs/functino.js
+++
@@ -1,5 +0,0 @@
-var urlOfCachedPage = "https://ferman557.github.io/Icegat/";
-var xhr = new XMLHttpRequest();
-xhr.open("GET", urlOfCachedPage, true);
-xhr.setRequestHeader("Cache-Control", "max-age=0");
-xhr.send();
